628b9b0ada0c8181f48031f3485ab9e032578c73
sur1029 <singhsurabhi1992@gmail.com>
2015-06-03 00:29:25 +0530

    line

---
+++ spec/geo_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'Calulate length test 1' do	
+
+	 it "return 5 for values (2,3) and (5,7)" do
+	 		@line = Line.new(2,3,5,7)
+	 		expect(@line.length).to eq(4)
+	 end
+
+	 it "return 0 for values (1,1) and (1,1)" do
+	 		@line = Line.new(1,1,1,1)
+	 		expect(@line.length).to eq(0)
+	 end
+end
